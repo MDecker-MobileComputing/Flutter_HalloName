ad8dc5df1818bf1a1f26991ce2116e4cdc18fcb1
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-08-02 18:14:45 +0200

    Update buildAndroid.bat

--- buildAndroid.bat
+++ buildAndroid.bat
@@ -1,5 +1,9 @@
 
-flutter build apk --split-per-abi
+@REM Eine FAT-APK-Datei wird erzeugt, die auf allen Android-Geraeten lauffaehig ist.
+flutter build apk
 
-@REM Bei Erfolg werden im folgenden Verzeichnis APK-Dateien abgelegt:
+@REM flutter build apk --split-per-abi
+@REM Drei APK-Dateien werden erzeugt: für ARM, ARM64 und x86.
+
+@REM Bei Erfolg wird im folgenden Verzeichnis die APK-Datei abgelegt:
 @REM build\app\outputs\apk\release
